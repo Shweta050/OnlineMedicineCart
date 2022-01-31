ef53f27e3edc89b5ede8c91b2d95428629822fd4
Lakshmikanthan <40634272+LakshmikanthanP@users.noreply.github.com>
2022-01-31 09:38:42 +0800

    Add category dropdown and fetch data from backend api

--- onlinemedicinecart/src/Components/Home.js
+++ onlinemedicinecart/src/Components/Home.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component ,useState} from 'react';
 import '../Styles/Home.css';
 
 import Footer from './Footer';
 import Navbar from './Navbar';
+import Select from 'react-select'
+import axios from 'axios'
+import Category from './Category';
 
 
-export default class Home extends Component {
-    render(){
+export default function Home() {
+
+   
 return(
-    <><Navbar/><Footer /></>
-);
-}
+    <><Navbar /><Footer /> < Category /></>
+)
 }
 
