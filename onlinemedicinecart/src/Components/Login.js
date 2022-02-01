7aaa4fa09e4e6398afef7bd26a20a2a35aa0c9c8
Rani, Shweta <shweta.rani@delta.com>
2022-02-01 13:13:11 +0530

    Login SignUp functionality added for all users

--- onlinemedicinecart/src/Components/Login.js
+++ onlinemedicinecart/src/Components/Login.js
@@ -1,13 +1,84 @@
-import React, { Component } from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import React, { Component } from 'react';
+import { Button, TextField, Link } from '@material-ui/core';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import '../Styles/Login.css'
+const axios = require('axios');
 
-export default class Login extends Component {
-    render() {
-        return (
-            <div>
-        <><Navbar/>Login Page<Footer/></>
-    </div>
-        )
-    }
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  login = () => {
+
+    axios.post('http://localhost:5000/login', {
+      email: this.state.email,
+      password: this.state.password,
+    }).then((res) => {
+      alert("Successful login!!");
+      console.log(res.name);
+    }).catch((err) => {
+        alert("Invalid Email ID or password")
+    });
+  }
+
+  render() {
+    return (
+        <>
+        <Navbar/>
+      <div style={{ marginTop: '100px' }}>
+        <div>
+          <h2>Login</h2>
+        </div>
+
+        <div>
+          <TextField
+            id="standard-basic"
+            type="text"
+            autoComplete="off"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+            placeholder="Email ID"
+            required
+          />
+          <br /><br />
+          <TextField
+            id="standard-basic"
+            type="password"
+            autoComplete="off"
+            name="password"
+            value={this.state.password}
+            onChange={this.onChange}
+            placeholder="Password"
+            required
+          />
+          <br /><br />
+          <Button
+            className="button_style"
+            variant="contained"
+            color="primary"
+            size="small"
+            disabled={this.state.name == '' && this.state.password == ''}
+            onClick={this.login}
+          >
+            Login
+          </Button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Link href="/signup">
+            SignUp
+          </Link>
+        </div>
+      </div>
+      <Footer/>
+      </>
+    );
+  }
 }
--- onlinemedicinecart/src/Components/Signup.js
+++ onlinemedicinecart/src/Components/Signup.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import axios from "axios";
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { Button, TextField, Link } from '@material-ui/core';
 
 const Signup = () => {
 
@@ -28,11 +29,11 @@ const Signup = () => {
         if( name && email && password && phone){
             axios.post("http://localhost:5000/signup", user)
             .then( res => {
-                alert(res.data.message)
+                alert("Successfully Signed Up!! Login now")
                 navigate('/login')
             })
         } else {
-            alert("invlid input")
+            alert("Please fill all the fields")
         }
         
     }
@@ -40,16 +41,27 @@ const Signup = () => {
     return (
       <>
       <Navbar/>
-        <div className="register">
+        <div className="register" style={{ marginTop: '100px'}}>
             {console.log("User", user)}
-            <h1>SignUp</h1>
-            <input type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></input>
-            <input type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
-            <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input>
-            <input type="password" name="phone" value={user.phone} placeholder="Your Phone Number" onChange={ handleChange }></input>
-            <div className="button" onClick={register} >Sign Up</div>
-            <div>or</div>
-            <div className="button" onClick={() => navigate('/login')}>Login</div>
+            <h1 style={{ marginTop: '100px'}}>SignUp</h1>
+            <input type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></input><br/>
+            <input type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input><br/>
+            <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input><br/>
+            <input type="text" name="phone" value={user.phone} placeholder="Your Phone Number" onChange={ handleChange }></input>
+            <br/><Button
+            className="button_style"
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={register}
+          >SignUp</Button><span> or </span>
+            <Button
+            className="button_style"
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={() => navigate('/login')}
+          >Login</Button>
         </div>
         <Footer/>
         </>
