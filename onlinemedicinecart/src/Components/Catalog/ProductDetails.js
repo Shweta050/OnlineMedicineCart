11662298b07e89b2ebf5f6b95fee4403d5afa0e4
Lakshmikanthan <40634272+LakshmikanthanP@users.noreply.github.com>
2022-02-11 01:16:38 +0800

    Working version

--- onlinemedicinecart/src/Components/Catalog/ProductDetails.js
+++ onlinemedicinecart/src/Components/Catalog/ProductDetails.js
@@ -3,17 +3,15 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch,useSelector } from "react-redux";
 import { listProductDetails } from "../../actions/productActions";
-// import {Loader} from '../Loader'
-// import {Message} from '../Message'
 
 export default function ProductDetails()
 {
     const dispatch = useDispatch();
 
     const productDetails = useSelector(state=>state.productDetails)
-    const {loading,error,product} = productDetails
+    const {loading,product} = productDetails
     const {id} = useParams();
-   console.log(id);
+    console.log(product)
 
     useEffect(()=>
     {
@@ -32,56 +30,58 @@ export default function ProductDetails()
         //   {/* {console.log(product[0].name)} */}
         // </Typography>
         <>
-        {loading? <h1>Loading..</h1>: error? <h2>{error}</h2>:(   
-                 <Grid container spacing={6}>
-             <Grid item xs={6}>
-                 <img src={product[0].image} alt={product[0].name} style={{width: '100%'}} />
-            </Grid>
-            <Grid item xs={6}>
-                <Typography variant='h3'>{product[0].name}</Typography>
-                <Divider sx={{mb: 2}} />
-                <Typography variant='h4' color='secondary'>{(product[0].price)} Rupees</Typography>
-                <TableContainer>
-                    <Table>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell>Name</TableCell>
-                                <TableCell>{product[0].name}</TableCell>
-                            </TableRow>    
-                            <TableRow>
-                                <TableCell>Description</TableCell>
-                                <TableCell>{product[0].description}</TableCell>
-                            </TableRow>  
-                            <TableRow>
-                                <TableCell>Category</TableCell>
-                                <TableCell>{product[0].category}</TableCell>
-                            </TableRow>  
-                            <TableRow>
-                                <TableCell>Brand</TableCell>
-                                <TableCell>{product[0].brand}</TableCell>
-                            </TableRow>  
-                            <TableRow>
-                                <TableCell>Quantity in stock</TableCell>
-                                <TableCell>{product[0].countInStock}</TableCell>
-                            </TableRow>  
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <Grid container spacing={2}>
+        {loading ?  <h1>Loading..</h1>:(
+                    <Grid container spacing={6}>
                     <Grid item xs={6}>
-                        {/* <TextField 
-                            variant='outlined'
-                            type='number'
-                            label='Quantity in Cart'
-                            fullWidth
-                            value={quantity}
-                            onChange={handleInputChange}
-                        /> */}
-                    </Grid>
+                        <img src={product[0].image} alt={product[0].name} style={{width: '100%'}} />
+                   </Grid>
+                   <Grid item xs={6}>
+                       <Typography variant='h3'>{product[0].name}</Typography>
+                       <Divider sx={{mb: 2}} />
+                       <Typography variant='h4' color='secondary'>{(product[0].price)} Rupees</Typography>
+                       <TableContainer>
+                           <Table>
+                               <TableBody>
+                                   <TableRow>
+                                       <TableCell>Name</TableCell>
+                                       <TableCell>{product[0].name}</TableCell>
+                                   </TableRow>    
+                                   <TableRow>
+                                       <TableCell>Description</TableCell>
+                                       <TableCell>{product[0].description}</TableCell>
+                                   </TableRow>  
+                                   <TableRow>
+                                       <TableCell>Category</TableCell>
+                                       <TableCell>{product[0].category}</TableCell>
+                                   </TableRow>  
+                                   <TableRow>
+                                       <TableCell>Brand</TableCell>
+                                       <TableCell>{product[0].brand}</TableCell>
+                                   </TableRow>  
+                                   <TableRow>
+                                       <TableCell>Quantity in stock</TableCell>
+                                       <TableCell>{product[0].countInStock}</TableCell>
+                                   </TableRow>  
+                               </TableBody>
+                           </Table>
+                       </TableContainer>
+                       <Grid container spacing={2}>
+                           <Grid item xs={6}>
+                               {/* <TextField 
+                                   variant='outlined'
+                                   type='number'
+                                   label='Quantity in Cart'
+                                   fullWidth
+                                   value={quantity}
+                                   onChange={handleInputChange}
+                               /> */}
+                           </Grid>
+       
+                       </Grid>
+                   </Grid>
+               </Grid>
+        )}
 
-                </Grid>
-            </Grid>
-        </Grid>)}
         </>
 
     )
--- onlinemedicinecart/src/Components/Catalog/ProductList.js
+++ onlinemedicinecart/src/Components/Catalog/ProductList.js
@@ -1,22 +1,33 @@
 import { Avatar, List, ListItem,ListItemText, ListItemAvatar, Grid } from "@material-ui/core";
 import ProductCard from "./ProductCard";
 import { useDispatch, useSelector } from "react-redux";
+import {Loader} from  '../Loader'
+import {Message} from '../Message'
 
 export default function ProductList(props)
 {
+    const productList = useSelector((state) => state.productList)
+    const { loading,error,products } = productList
 
     return(
+        <>
+        {loading? <h1>Loading..</h1>: error? <h2>{error}</h2>:(
         <Grid container spacing={4}>
-                {console.log(props.products)}    
+        {console.log(props.products)}    
         {props.products.map(item=>(
-            <Grid item xs={4}>
-                        { 
-                            <ProductCard key = {item._id} product={item} />                
-                        }
-            </Grid>
-        )
+         <Grid item xs={4}>
+                { 
+                    <ProductCard key = {item._id} product={item} />                
+                }
+        </Grid>
+)
 
-    )}
-    </Grid>
+)}
+</Grid>
+        )}
+
+
+        </>
+
     );
 }
